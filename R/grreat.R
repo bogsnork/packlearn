0363199382a6d4a881f6a4d65f26b05372cc9da5
CK <christoph.kratz@naturalengland.org.uk>
2021-10-10 12:26:23 +0100

    added documentation

--- R/grreat.R
+++ R/grreat.R
@@ -1,3 +1,17 @@
+#' Praise food
+#'
+#' Print a comment to the console that praises the food you specify like Tony the Tiger.
+#'
+#' @param food String naming a food
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' grreat(food = "Okra")
+
 grreat <- function(food) {
   print(paste(food, "is grrrreat!"))
 }
+
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(packlearn)
+
+test_check("packlearn")
